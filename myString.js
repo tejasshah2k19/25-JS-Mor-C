f477a2736324427ef807503530b7af628b5a48ea
Tejas Shah <tejasshah2k19@gmail.com>
2025-10-08 09:56:42 +0530

    string

---
+++ myString.js
@@ -0,0 +1,42 @@
+console.log("String : demo");
+
+
+
+let a = "royal education";
+
+//how many characters you have in string 
+console.log(a.length);//15 
+console.log(a[0]);
+console.log(a.charAt(0));// char at 0 
+
+
+console.log(a.toUpperCase());//method first letter small ,second word first character upper , third ,fourth
+console.log(a.toLowerCase());
+
+
+let str = "jony jony yes papa jony";
+
+console.log(str.includes("r"));//false 
+console.log(str.includes("jony"));//true
+
+console.log(str.indexOf("a"));
+
+console.log(str.indexOf("papa"));
+
+console.log(str.indexOf("jony"));
+
+console.log(str.lastIndexOf("jony"));
+
+
+//count how many digits in the string 
+//count how many vowels in the string 
+//reverse the string 
+
+
+
+
+
+
+
+
+
---
+++ typeOfDemo.js
@@ -0,0 +1,24 @@
+console.log("this is external js");
+
+
+let a = 10;
+let b = 10.5; 
+let c = 'x'; 
+let d = "royal"; 
+let e = false; 
+let f = [] ;  
+
+
+console.log(typeof(a));//number
+console.log(typeof(b));//number 
+
+console.log(typeof(c));//string '' 
+console.log(typeof(d));//string ""
+
+console.log(typeof(e));//boolean : true , false 
+
+console.log(typeof(f));//object
+
+
+
+
